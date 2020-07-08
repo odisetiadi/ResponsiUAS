2f6e6de108649e78c1d275ccd36b08ca181e467a
Dwi Putra Setiadi <61934529+odisetiadi@users.noreply.github.com>
2020-07-08 15:41:28 +0700

    Add files via upload

---
+++ Penjualan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Penjualan
+    {
+        public double Nota { get; set; }
+        public string Tanggal { get; set; }
+        public string Customer { get; set; }
+        public string Jenis { get; set; }
+        public double TotalNota { get; set; }
+
+        // PERINTAH: lengkapi property class penjualan, sesuai petunjuk soal
+    }
+}
